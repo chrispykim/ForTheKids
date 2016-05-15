54ca28ca73a5d1e58d3ae6974f49c100742fe55c
jkhoh <jkhoh@ucsd.edu>
2016-05-15 14:11:50 -0700

    scoreboard

---
+++ Assets/Scripts/budget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class budget : MonoBehaviour {
+    public GUIText budget;
+    public int money;
+
+	// Use this for initialization
+	void Start () {
+        money = 0;
+        budget.text = "Budget: " + money.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class score : MonoBehaviour {
+    public GUIText budget;
+    public int money;
+
+	// Use this for initialization
+	void Start () {
+        money = 0;
+        budget.text = "Budget: " + money.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score:  " + score;
+    }
+}
